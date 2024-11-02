15d167ace6ad84dd9d6750480146cdcc6eda2b0e
danielMieleniewski <danielMieleniewski@outlook.com>
2024-11-02 15:36:16 +0000

    Add files via upload
    First upload

---
+++ CONSTRUCTOR ASSIGNMENT/CONSTRUCTOR ASSIGNMENT/Chain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CONSTRUCTOR_ASSIGNMENT
+{
+    public class Chain
+    {
+        public Chain(float num1) : this(num1, "Bob")
+        {
+        }
+
+        public Chain(string name1) : this(42, name1)
+        {
+            Console.WriteLine(name1 + " and our favorite number is " + Num);
+        }
+
+        public Chain(float num, string name)
+        {
+            Name = name;
+            Num = num; // Fixed assignment here
+        }
+
+        public string Name { get; set; } // Made public for external access
+        public float Num { get; set; }   // Made public for external access
+    }
+}
---
+++ CONSTRUCTOR ASSIGNMENT/CONSTRUCTOR ASSIGNMENT/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CONSTRUCTOR_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string const1 = "Your Name is:";
+            const string const2 = "Your favorite number is:";
+            Console.WriteLine("Enter your name or favorite number:");
+            var answer = Console.ReadLine();
+            if (float.TryParse(answer, out float x))
+            {
+                Console.WriteLine(const2);
+                Chain newChain = new Chain(x);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(const1);
+                Chain newChain = new Chain(answer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
